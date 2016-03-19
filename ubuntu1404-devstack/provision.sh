91ad0000287306d993702565386a72701e1ed012
Anil Belur <askb23@gmail.com>
2016-03-19 19:38:47 +0530

    created a localrc.conf and modified the vagrantfile to checkout devstack from git url
    Signed-off-by: Anil Belur <askb23@gmail.com>

--- ubuntu1404-devstack/provision.sh
+++ ubuntu1404-devstack/provision.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
-devstackversion="devstack-liberty"
+devstackversion="devstack-kilo"
 
-# make sure the packages are installed
-# sudo apt-get udpate -y
-# sudo apt-get upgrade -y
-# sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
-# sudo apt-get install -y git
-# sudo apt-get install -y wget
-# sudo apt-get install -y python-pip
-# sudo apt-get install -y python-dev
-
-# clone and build devstack directly from git
-# git clone -b stable/kilo https://github.com/openstack-dev/devstack.git
-# devstackversion="devstack"
-# cd ~/${devstackversion}
+clone and build devstack directly from git
+git clone -b stable/kilo https://github.com/openstack-dev/devstack.git $i{devstackversion}
+cd ~/${devstackversion}
 
 # create a stack user for building devstack - fix this
 sudo bash /shared/${devstackversion}/tools/create-stack-user.sh
 sudo su -l vagrant
 
-# fixes - [ERROR] Can't find package os-testr in requirements - if pip is not upgraded
-# sudo pip install --upgrade pip
-# sudo pip install -U os-testr
-
-# build devstack from share
+# build devstack from shared folder
 # cp -rpf /shared/${devstackversion} ~/
-cd /shared/
-tar cf - ./${devstackversion} | ( cd /home/vagrant/ && tar xvf - )
-cp /shared/local.conf ~/${devstackversion}/local.conf
+# cd /shared/
+# tar cf - ./${devstackversion} | ( cd /home/vagrant/ && tar xvf - )
+# cp /shared/local.conf ~/${devstackversion}/local.conf
 #chown stack:stack ~/${devstackversion} -R
-chown vagrant:vagrant ~/${devstackversion} -R
-mkdir /opt/stack
-chmod 770 /home/vagrant/${devstackversion} -R
-chmod 770 /opt/stack -R
-cd ~/${devstackversion}
+# chown vagrant:vagrant ~/${devstackversion} -R
+# mkdir /opt/stack
+# chmod 770 /home/vagrant/${devstackversion} -R
+# chmod 770 /opt/stack -R
+# chown vagrant:vagrant /opt/stack -R
+# cd /home/vagrant/${devstackversion}
 
-# Replace Host IP
+# copy the local.conf into the devstack folder
+cp ~/local.conf ~/${devstackversion}
+
+# Replace Host IP in the localrc
 sed -i '/HOST_IP/ c HOST_IP=10.0.2.15' local.conf
 
 # start building devstack
 bash ./stack.sh
 
-echo "Finished setting up devstack."
+echo "Finished setting up devstack, this should give your the dashboard login"
+echo "admin/demo credential in the local.conf"
